f10fd738ffe43e223b9ed1fc9a1c9c82e96b7c2d
Markus Domenegheti <markusvdc@gmail.com>
2026-09-07 19:30:05 -0300

    Refine global options and lower bee nest chance

--- src/main/java/br/com/capfood/mixin/BeehiveDecoratorMixin.java
+++ src/main/java/br/com/capfood/mixin/BeehiveDecoratorMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(BeehiveDecorator.class)
 public abstract class BeehiveDecoratorMixin {
 	private static final float SAPLING_BEE_NEST_CHANCE = 0.05F;
-	private static final float INCREASED_SAPLING_BEE_NEST_CHANCE = 0.20F;
+	private static final float INCREASED_SAPLING_BEE_NEST_CHANCE = 0.10F;
 
 	@ModifyExpressionValue(
 		method = "place",
